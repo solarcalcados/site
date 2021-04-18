2cd1de076b61b5591e1c670ef2e8a56aa3e7f3b4
VKyobV <82035186+solarcalcados@users.noreply.github.com>
2021-04-18 20:10:30 -0300

    d

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
 console.log("firebase.auth()")
 console.log(firebase)
-firebase.auth().createUserWithEmailAndPassword("email", "password")
-.then((userCredential) => {
-  // Signed in
-  var user = userCredential.user;
-  console.log("user")
-  // ...
-})
-.catch((error) => {
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  console.log(errorCode+""+errorMessage);
-  // ..
-});
+firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
