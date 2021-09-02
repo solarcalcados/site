84abf38cd98fd7a8b8ed6985112e10a27676f06c
VKyobV <82035186+solarcalcados@users.noreply.github.com>
2021-09-02 16:28:02 -0300

    as

--- js/acessoindex.js
+++ js/acessoindex.js
@@ -22,8 +22,9 @@ firebase.auth().onAuthStateChanged((user) => {
 
       db.collection("products").get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                console.log(doc.id, " => ", doc.data());
+                
+                
+                console.log(doc.id, " => ", doc.data().marca);
             });
         });
 
