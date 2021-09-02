7aebae3a77fbb113b8eaaea5d78154f1c50a04ba
VKyobV <82035186+solarcalcados@users.noreply.github.com>
2021-09-02 14:21:13 -0300

    botão

--- js/acessoindex.js
+++ js/acessoindex.js
@@ -18,11 +18,19 @@ var db = firebase.firestore();
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
+      
+
       var uid = user.uid;
       // ...
     } else {
       // User is signed out
-      // ...
+      window.location.href = "http://www.solarcalcados.com.br/acesso.html";
     }
   });
+function logout(){
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+      }).catch((error) => {
+        // An error happened.
+      });
+}
