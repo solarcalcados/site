4aeffe5e2976f5e4f34528eda63ada70d0fcdb4c
VKyobV <82035186+solarcalcados@users.noreply.github.com>
2021-09-02 15:03:32 -0300

    as

--- js/acessoindex.js
+++ js/acessoindex.js
@@ -18,9 +18,9 @@ var db = firebase.firestore();
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // User is signed in, see docs for a list of available properties
-      console.log(uid)
-
       var uid = user.uid;
+
+      console.log(uid)
       // ...
     } else {
       // User is signed out
